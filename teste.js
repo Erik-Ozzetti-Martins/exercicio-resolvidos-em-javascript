3445faa21e1bb9490e4cd6dd557a580f4240e603
Erik Ozzetti Martins <77300089+Erik-Ozzetti-Martins@users.noreply.github.com>
2021-06-07 13:04:13 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,49 +1,21 @@
-// let par = 6;
-// for (let i = 1 ;i <=par;i++ ){
-//   if (i % 2 ==0){
-//     console.log(i)
-//   }
-// }  
 
-let lista = [-5, 0, -3, -4,12];
 
-function verifica(lista){
-    let par = [];
-    let impar = [];
-    let positivos = [];
-    let negativos = [];
-    for (let i =0; i < lista.length; i++){
-        if(lista[i]>0 || lista[i] ===0){
-            positivos.push(lista[i])           
-        }else if (lista[i]< 0 ){
-            negativos.push(lista[i])
-        }else if(lista[i] % 2== 0){
-            par.push(lista[i])
-        }else if(lista[i]%2 !=0){
-            impar.push(lista[i])
-        }    
-    }
-    
-   
-    console.log(positivos)
-    console.log(negativos)
+let lista = [-5, 0, -3, -4, 12 ,3];
+
+function verifica(lista) {
+  let par = lista.filter((value) => {
+    return value % 2 === 0;
+  });
+  let impar = lista.filter((value) => {
+    return value % 2 !== 0;
+  });
+  let positivo = lista.filter((value) => {
+    return value >= 0;
+  });
+  let negativo = lista.filter((value) => {
+    return value <= 0;
+  });
+
+  return `numeros pares ${par}, numeros impares ${impar}, numeros positivos ${positivo}, numeros negativos ${negativo} `
 }
-
-
-
-function parOuImpar (lista){
-    let par = [];
-    let impar = [];
-  
-    for (let i =0; i < lista.length; i++){       
-        if(lista[i] % 2== 0){
-            par.push(lista[i])
-        }else if(lista[i]%2 !=0){
-            impar.push(lista[i])
-        }    
-    }
-    
-    console.log(par);
-    console.log(impar);
-}    
-parOuImpar(lista)
+console.log(verifica(lista));
